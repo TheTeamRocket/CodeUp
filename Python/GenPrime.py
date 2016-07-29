ca79e58fb6a3a6b723e3ef30c34dd83bfb1294f0
Shashank Gupta <shashank.gupta2303@gmail.com>
2016-07-29 15:32:02 +0530

    Update GenPrime.py

--- Python/GenPrime.py
+++ Python/GenPrime.py
@@ -1,6 +1,6 @@
 def GenPrime(N)
-  p=[2,3]+filter(lambda x:((x+1)%6==0)or((x-1)%6==0),range(2,N/2+1))
+  p=[2,3]+filter(lambda x:((x+1)%6==0)or((x-1)%6==0),range(2,N))
     for i in p:
         p=filter(lambda x:(x%i!=0)or(x==i),p)
-        if(p[-1]<i**2):
+        if(p[-1]<i):
             break
